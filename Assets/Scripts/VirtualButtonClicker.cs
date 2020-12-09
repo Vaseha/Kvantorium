def1a454b1f7ce21e54880ab391786160172bc46
Василий Халиулов <vaseha50@gmail.com>
2020-12-09 13:42:13 +0300

    remake ResourceCollector

---
+++ Assets/Scripts/VirtualButtonClicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonClicer : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject Сube,VirtualButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //VirtualButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        GameObject.Find ("Сube");
+        Сube.SetActive (false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Сube.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Сube.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
